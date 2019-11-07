3a54e2320f00155324e0671e5a7e509cc936d7a4
dominiquetrindade <mick22009@hotmail.com>
2019-11-07 10:36:01 -0300

    enviando tudo

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask, render_template
+from formulario import 
 
 app = Flask(__name__)
+app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///msgs.db'
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/formulario')
+@app.route('/formulario', methods=[POST, GET])
 def formulario():
-    return render_template('formulario.html')
+    form = MsgForm()
 
-app.run(debug=True)
+    return render_template('formulario.html', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
